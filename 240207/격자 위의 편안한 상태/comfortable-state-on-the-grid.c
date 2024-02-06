c79fb5a5f41ea3b8b96548d73871b26bf666d676
LeMoN <77883115+answjddns08@users.noreply.github.com>
2024-02-07 00:45:29 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240207/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+_Bool checking(int r,int c,int n,_Bool map[100][100]);
+
+int main(void)
+{
+    int n,m;
+    _Bool map[100][100] = { 0, };
+    int r,c;
+
+    scanf("%d %d",&n,&m);
+
+    for (int i = 0;i < m;i++)
+    {
+        scanf("%d %d",&r,&c);
+
+        //printf("intput: %d %d\n",r,c);
+
+        r--;
+        c--;
+
+        map[r][c] = 1;//true랑 같음
+
+        printf("%d\n",checking(r,c,n,map));
+
+        
+    }
+
+    return 0;
+}
+
+_Bool checking(int r,int c,int n,_Bool map[100][100])
+{
+    int cnt;
+
+    if (r + 1 <= n && map[r + 1][c])
+    {
+        cnt++;
+    }
+
+    if (r - 1 >= 0 && map[r - 1][c])
+    {
+        cnt++;
+    }
+
+    if (c + 1 <= n && map[r][c + 1])
+    {
+        cnt++;
+    }
+
+    if (c - 1 >= 0 && map[r][c - 1])
+    {
+        cnt++;
+    }
+
+    //printf("%d\n",cnt);
+
+    if (cnt == 3)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
