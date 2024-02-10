ac414713aef747678994e72cd685bb8302e56835
LeMoN <77883115+answjddns08@users.noreply.github.com>
2024-02-10 23:15:56 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240210/빙빙 돌며 사각형 채우기/snail-alphabet-square.c
+++ 240210/빙빙 돌며 사각형 채우기/snail-alphabet-square.c
@@ -21,7 +21,7 @@ int main(void)
     {
         map[y][x] = temp + 65;
         
-        temp = temp > 25 ? 0 : temp + 1;
+        temp = temp >= 25 ? 0 : temp + 1;
         //printf("%d %d\n",y,x);
 
         switch (dir)
