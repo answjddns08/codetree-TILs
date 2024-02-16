0ed77e7b8f5c9f201ed75e9f0989edcb1efc7cbf
LeMoN <77883115+answjddns08@users.noreply.github.com>
2024-02-17 06:39:31 +0900

    Title: 모두 3의 배수, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240217/모두 3의 배수/all-multiples-of-3.py
+++ 240217/모두 3의 배수/all-multiples-of-3.py
@@ -1,9 +1,9 @@
-a = list(map(int,input().split('\n')))
-
 is3 = True
 
-for i in a:
-    if i % 3 != 0:
+for i in range(5):
+    a = int(input())
+
+    if a % 3 != 0:
         is3 = False
         break
 
