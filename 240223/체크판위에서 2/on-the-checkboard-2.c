d3341eff6abbac715963885bc368361b77f696df
LeMoN <77883115+answjddns08@users.noreply.github.com>
2024-02-23 03:40:02 +0900

    Title: 체크판위에서 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/체크판위에서 2/on-the-checkboard-2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool map[15][15] = {0};
+int r,c;
+int sum = 0;
+
+void Function(int y,int x,bool pre,int cnt);
+
+int main(void)
+{
+    char input;
+
+    scanf("%d %d ",&r,&c);
+
+    for (int i = 0;i < r;i++)
+    {
+        for (int j = 0;j < c;j++)
+        {
+            scanf("%c ",&input);
+            map[i][j] = input == 'B' ? false : true;
+        }
+    }
+
+    Function(0,0,map[0][0],0);
+
+    printf("%d",sum);
+
+    return 0;
+}
+
+void Function(int y,int x,bool pre,int cnt)
+{
+
+    if (cnt == 3)
+    {
+        sum++;
+    }
+    else
+    {
+        for (int i = y + 1;i < r;i++)
+        {
+            for (int j = x + 1;j < c;j++)
+            {
+                if (map[i][j] != pre)
+                    {
+                        //printf("%d %d %d\n",i,j,cnt);
+                        Function(i,j,map[i][j],cnt + 1);
+                    }
+            }
+        }
+    }
+}
